090c6cfd5d076ebd07eb7b4d83f91ceeeedb00d3
Pablo Biajoni <pbiajoni@hotmail.com>
2019-08-29 16:01:05 -0300

    commit de segurança

---
+++ MyConnector.Tests/DatabaseTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyConnector.Mapping;
+
+namespace MyConnector.Tests
+{
+    [TestClass]
+    public class DatabaseTests
+    {
+        [TestMethod]
+        public void TestGetTable()
+        {
+            Database database = new Database("database");
+            Table table = new Table("table");
+            table.Fields.Add(new Field("field", "varchar(100)"));
+            database.Tables.Add(table);
+
+            Assert.AreEqual(table, database.GetTable("table"));
+        }
+    }
+}
